fd43cc6c5bcd096caccdd7f51b553eaf4455c55b
surpuredhanashri <surpuredhanashri@gmail.com>
2020-09-25 08:45:49 +0530

    done

---
+++ app.js
@@ -0,0 +1,20 @@
+let button = document.getElementById("theboxes");
+button.addEventListener("click",myFunction);
+
+var oldvalue = 0;
+var i= 0;
+function myFunction(){
+    let n = document.getElementById('number').value;
+    n = parseInt(oldvalue) + parseInt(n);
+    for(i=1; i<=n; i++){
+        var box = document.createElement('div');
+        box.classList.add('mydiv');
+        document.getElementById('box').appendChild(box);
+        box.innerHTML=1;
+    }
+
+    oldvalue = document.getElementById('box').lastElementChild.innerHTML;
+    console.log(oldvalue)
+
+
+}
